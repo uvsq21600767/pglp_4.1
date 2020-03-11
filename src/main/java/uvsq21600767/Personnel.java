cc04b9108121a35a1401dfcd7a33447ccdeff4a7
Thomas <thomas.cocquebert@ens.uvsq.fr>
2020-03-11 10:35:55 +0100

    Ajout de Personnel.java

---
+++ src/main/java/uvsq21600767/Personnel.java
@@ -0,0 +1,46 @@
+package uvsq21600767;
+
+public final class Personnel {
+    private final String nom;
+    private final String prenom;
+    private final java.time.LocalDate dateNaissance;
+    private final String tel;
+    private final String fonction;
+
+    public static class Builder {
+        private final String nom;
+        private final String prenom;
+        private final java.time.LocalDate dateNaissance;
+
+        private String tel = "";
+        private String fonction = "";
+
+        public Builder(String prenom, String nom, java.time.LocalDate dateNaissance) {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.dateNaissance = dateNaissance;
+        }
+
+        public Builder tel(String num) {
+            tel = num;
+            return this;
+        }
+
+        public Builder fonction(String func) {
+            fonction = func;
+            return this;
+        }
+
+        public Personnel build() {
+            return new Personnel(this);
+        }
+    }
+
+    private Personnel(Builder build) {
+        nom = build.nom;
+        prenom = build.prenom;
+        dateNaissance = build.dateNaissance;
+        tel = build.tel;
+        fonction = build.fonction;
+    }
+}
